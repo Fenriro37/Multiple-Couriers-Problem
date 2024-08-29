2530a6e695b90142254e1960bd7e60b8c0b904e7
point516 <turekhasimusa3@gmail.com>
2024-08-30 00:25:19 +0500

    run on 10 instances

--- SAT/SAT.py
+++ SAT/SAT.py
@@ -2,6 +2,7 @@ from z3 import *
 from itertools import combinations
 from collections import defaultdict
 import time
+import json
 
 #Naive
 def at_least_one(bool_vars):
@@ -119,7 +120,12 @@ def calculate_lower_bound(D):
         
     return lower_bound
 
-def calculate_upper_bound(D, n):
+def calculate_upper_bound(D, n, m):
+    if n%m != 0:
+        stops = n//m+1
+    else:
+        stops = n//m
+
     # Flatten the matrix into a 1D list
     flat_D = [distance for row in D for distance in row]
     
@@ -127,9 +133,152 @@ def calculate_upper_bound(D, n):
     sorted_D = sorted(flat_D, reverse=True)
     
     # Return the first n values
-    return sum(sorted_D[:n])
+    return sum(sorted_D[:stops+1])
 
-def solve_mcp(m, n, l, s, D, timeout = 300):
+def solve_mcp_nosym(m, n, l, s, D, timeout = 300):
+    start = time.time()
+    iter = 0
+    satisfiable = True
+    best_value = 0
+    best_solution = {}
+
+
+    solver = Solver()
+    solver.set("timeout", 300000)
+
+    # Define variables
+    # x[i][j][k] is True if courier i visits point j as their k-th stop
+    x = [[[Bool(f'x_{i}_{j}_{k}') for k in range(n+2)] for j in range(n+1)] for i in range(m)] # n+1 or n+2 ?
+
+    # Constraints
+    # 1. Each courier starts and ends at the depot (point n+1)
+    for i in range(m):
+        solver.add(x[i][n][0])  # Start at depot
+        solver.add(x[i][n][n+1])  # End at depot
+    
+    # 2. Each courier visits exactly one point at each step
+    for i in range(m):
+        for k in range(n+2):
+            solver.add(exactly_one([x[i][j][k] for j in range(n+1)], name=f"visit_e1_{i}_{k}"))
+    
+    # 3. Each point (except depot) is visited at most once by each courier SEEMS LIKE REDUNDANT
+    # for i in range(m):
+    #     for j in range(n):  # Exclude depot
+    #         solver.add(at_most_one([x[i][j][k] for k in range(n+2)], name=f"visit_m1_{i}_{j}"))
+    
+    # 4. Each point (except depot) is visited exactly once by all couriers combined
+    for j in range(n):  # Exclude depot
+        solver.add(exactly_one([x[i][j][k] for i in range(m) for k in range(1,n+1)], name=f"point_e1_{j}"))
+    
+    # 5. Respect load size for each courier
+    for i in range(m):
+        solver.add(Sum([If(x[i][j][k], s[j], 0) for j in range(n) for k in range(1,n+1)]) <= l[i])
+    
+    # 6. At least one stop
+    # New constraint: Each courier must leave the origin (make at least one stop)
+    # for i in range(m):
+    #     solver.add(at_least_one([x[i][j][1] for j in range(n)]))  # j from 0 to n (non-depot locations)
+    
+    # testing(goodfor7th)
+    # for i in range(m):
+    #     solver.add(at_least_one([x[i][j][2] for j in range(n)]))  # j from 0 to n (non-depot locations)
+
+    loads = list(enumerate(l))
+    
+    # Sort the list of tuples based on the number (in descending order)
+    sorted_indexed_numbers = sorted(loads, key=lambda x: x[1], reverse=True)
+    
+    # Extract the sorted numbers and their original indices
+    sorted_loads = [idx for idx, _ in sorted_indexed_numbers]
+
+    # Fair division of loads
+    for fair in range(1,(n//m)+1): 
+        for i in range(m):
+            solver.add(at_least_one([x[i][j][fair] for j in range(n)]))  # j from 0 to n (non-depot locations)
+    
+    if n%m != 0:
+        for i in range(n%m):
+            solver.add(at_least_one([x[sorted_loads[i]][j][fair+1] for j in range(n)]))  # j from 0 to n (non-depot locations)
+
+    # New constraint: Ensure consecutive stops
+    for i in range(m):
+        for k in range(1,n+1):
+            for j in range(n):  # For each non-depot location
+                solver.add(Implies(
+                    x[i][j][k],  # If courier i is at location j at step k
+                    Or(
+                        Or([x[i][j2][k+1] for j2 in range(n)]),  # Next step is another non-depot location
+                        x[i][n][k+1]  # Or next step is the depot
+                    )
+                ))
+        
+        # Ensure that after visiting depot, all remaining stops are also depot
+        for k in range(1,n+1):
+            solver.add(Implies(
+                x[i][n][k],
+                And([x[i][n][k2] for k2 in range(k+1, n+2)])
+            ))
+
+    # Calculate distances and set max_distance
+    max_distance = Int('max_distance')
+    courier_distances = []
+    for i in range(m):
+        # Distance for the route
+        route_distance = Sum([If(And(x[i][j1][k], x[i][j2][k+1]), D[j1][j2], 0) 
+                            for j1 in range(n+1) for j2 in range(n+1) for k in range(n+1)])
+        
+        courier_distances.append(route_distance)
+    
+    for distance in courier_distances:
+        solver.add(max_distance >= distance)
+
+    # Add a constraint to make max_distance as small as possible
+    solver.add(Or([max_distance == distance for distance in courier_distances]))
+
+    # set lower and upper bounds ########################### Maybe some implied constraints such that I do not need to add constraints for fair division since bounds take care of that?
+    solver.add(max_distance>=calculate_lower_bound(D))
+    solver.add(max_distance<=calculate_upper_bound(D, n, m))
+
+    # Minimize max_distance
+    # solver.minimize(max_distance)
+    
+
+    while satisfiable:
+        status = solver.check()
+        try_time = int(time.time() - start)
+        
+        if status == unsat:
+            if iter == 0:
+                print("unsat")
+                raise ValueError
+            else:
+                satisfiable = False
+        elif status == sat and timeout - try_time > 0:
+            iter += 1
+            model = solver.model()
+            best_value = model.eval(max_distance)
+            # print(iter)
+            # print(best_value)
+            best_solution = get_dict(try_time, model, best_value, x, False)
+            solver.push()
+            solver.add(max_distance < best_value)
+            solver.set("timeout", 300000-(try_time*1000))
+            # return model, max_distance, x
+        if timeout - try_time <= 0:
+            optimal = False
+            if iter == 0:
+                raise TimeoutError
+            else:
+                return best_solution     
+            
+    best_solution['optimal'] = True
+    # model = solver.model()
+    # best_value = model.eval(max_distance)
+    # best_solution = get_dict(try_time, model, best_value, x, optimal)
+    return best_solution
+
+def solve_mcp_sym(m, n, l, s, D, timeout = 300):
+    start = time.time()
     iter = 0
     satisfiable = True
     best_value = 0
@@ -257,16 +406,15 @@ def solve_mcp(m, n, l, s, D, timeout = 300):
 
     # set lower and upper bounds ########################### Maybe some implied constraints such that I do not need to add constraints for fair division since bounds take care of that?
     solver.add(max_distance>=calculate_lower_bound(D))
-    solver.add(max_distance<=calculate_upper_bound(D, (n//m)+1))
+    solver.add(max_distance<=calculate_upper_bound(D, n, m))
 
     # Minimize max_distance
     # solver.minimize(max_distance)
     
-    start = time.time()
 
     while satisfiable:
         status = solver.check()
-        try_time = time.time() - start
+        try_time = int(time.time() - start)
         
         if status == unsat:
             if iter == 0:
@@ -278,36 +426,65 @@ def solve_mcp(m, n, l, s, D, timeout = 300):
             iter += 1
             model = solver.model()
             best_value = model.eval(max_distance)
-            print(iter)
-            print(best_value)
+            # print(iter)
+            # print(best_value)
             best_solution = get_dict(try_time, model, best_value, x, False)
             solver.push()
             solver.add(max_distance < best_value)
+            solver.set("timeout", 300000-(try_time*1000))
             # return model, max_distance, x
-        if timeout - try_time < 0:
+        if timeout - try_time <= 0:
             optimal = False
             if iter == 0:
                 raise TimeoutError
             else:
                 return best_solution     
             
-    optimal = True
+    best_solution['optimal'] = True
     # model = solver.model()
     # best_value = model.eval(max_distance)
     # best_solution = get_dict(try_time, model, best_value, x, optimal)
     return best_solution
 
 if __name__ == '__main__':
-    filename = r"C:\Users\turek_hd7cu13\coding\gavno_gavna\Instances\inst07.dat"
-    m, n, l, s, D = read_dat_file(filename)
+    names = ['1','2','3','4','5','6','7','8','9','10']
+    instances = ["01","02","03","04","05","06","07","08","09","10"]
+    for index, inst in enumerate(instances[0:5]):
+        filename = f"../instances/inst{inst}.dat"
+        m, n, l, s, D = read_dat_file(filename)
 
-    out = solve_mcp(m, n, l, s, D)
-    model = out['model']
-    x = out['x']
+        out = solve_mcp_sym(m, n, l, s, D)
+        model = out['model']
+        x = out['x']
+        tours_sym = []
 
-    print(f"Maximum distance: {out['distance']}")
-    for i in range(m):
-        tour = [j for k in range(n+2) for j in range(n+1) if model.eval(x[i][j][k])]
-        print(f"Courier {i+1}: Tour {tour}")
-    print(f"Time: {out['time']}")
-    print(f"Optimal? : {out['optimal']}")
+        for i in range(m):
+            tour = [j+1 for k in range(n+2) for j in range(n+1) if model.eval(x[i][j][k]) and j != n]
+            tours_sym.append(tour)
+
+        out_nosym = solve_mcp_nosym(m, n, l, s, D)
+        model = out_nosym['model']
+        x = out_nosym['x']
+        tours_nosym = []
+
+        for i in range(m):
+            tour = [j+1 for k in range(n+2) for j in range(n+1) if model.eval(x[i][j][k]) and j != n]
+            tours_nosym.append(tour)
+
+        out = {
+            "SAT":{
+            "time": int(out_nosym['time']),
+            "optimal": out_nosym['optimal'],
+            "obj": int(str(out_nosym['distance'])),
+            "sol": tours_nosym
+            },
+            "SAT_symbreak":{
+            "time": int(out['time']),
+            "optimal": out['optimal'],
+            "obj": int(str(out['distance'])),
+            "sol": tours_sym
+            }
+        }
+
+        with open(f"../res/SAT/{names[index]}.json","w") as file:
+            json.dump(out, file, indent = 4)
